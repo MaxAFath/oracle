45c4f5ae85a82b959dfafda3f2bacbfefdf90e8b
Max Fathauer <maxfathauer@gmail.com>
2022-07-20 19:23:21 -0400

    schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,9 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers ={
+    Query:{},
+    Mutation:{}
+};
+
+module.exports resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,3 @@
+const { typeDefs } = require("apollo-server-express");
+
+module.exports = typeDefs;
