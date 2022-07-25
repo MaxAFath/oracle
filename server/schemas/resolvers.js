92b42de747fb5afa80008c6b8e341e8d5e80b9d1
Max Fathauer <maxfathauer@gmail.com>
2022-07-25 19:16:31 -0400

    fixed resolver error

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,16 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
+const {User} = require('../models');
+
 
 const resolvers ={
-    Query:{},
-    Mutation:{}
+    Mutation:{
+        addUser: async(parent, args)=>{
+            const user = await User.create(args);
+            const token = signToken(user);
+            return {token, user};
+        }
+    }
 };
 
-module.exports resolvers;
+module.exports = resolvers;
