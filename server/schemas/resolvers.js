8a299e78ee39349f20eec4e99a5a417d40f7f619
Max Fathauer <maxfathauer@gmail.com>
2022-07-25 20:26:48 -0400

    bug fix

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,7 @@ const { User } = require('../models');
 
 
 const resolvers = {
+    Query:{},
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
