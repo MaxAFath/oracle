575274903ed5c8ad71e9ed7996cbcf84ed5d1452
Max Fathauer <maxfathauer@gmail.com>
2022-07-30 08:32:01 -0400

    hidden merge conflict resolved

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,16 +1,6 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import axios from 'axios';
-<<<<<<< HEAD
-const client = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup';
-const host = 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com';
-const key = 'c1ad77c2admsh5b84afab4ae55c6p1137bbjsn2f03e9a2c7cf';
-function SearchBar() {
-
-    const axios = require("axios");
-    state = {
-        results: []
-=======
 //import mediaSearch from '../../utils/lookup';
 
 
@@ -38,7 +28,6 @@ function SearchBar() {
             console.error(error);
         });
         return result;
->>>>>>> develop
     }
     document.getElementById("submit").click(function (event) {
         event.preventDefault();
@@ -47,33 +36,6 @@ function SearchBar() {
         console.log(mediares);
     })
 
-<<<<<<< HEAD
-
-
-    const options = {
-        method: 'GET',
-        url: client,
-        params: { term: 'bojack', country: 'us' },
-        headers: {
-            'X-RapidAPI-Key': key,
-            'X-RapidAPI-Host': host
-        }
-    };
-
-    axios.request(options).then(function (response) {
-        console.log(response.data);
-        const result = response.data;
-        this.setState({ results });
-    }).catch(function (error) {
-        console.error(error);
-    });
-    if (Auth.loggedIn()) {
-        return (
-            <div>
-                <label>Search to see where your show is streaming</label>
-                <input id="show" type="text" size="42"></input>
-                <input id="user-submit" class="submit-quote-button" type="submit" value="Submit"></input>
-=======
     if (Auth) {
         return (
             <div className="search">
@@ -89,7 +51,6 @@ function SearchBar() {
         return(
             <div className="search">
                 <h2 className="warning">You must sign in to have acces to search functionality</h2>
->>>>>>> develop
             </div>
         )
     }
