9f6636feb8ddaea2158f3102869654e7f3b62435
Max Fathauer <maxfathauer@gmail.com>
2022-07-30 00:04:49 -0400

    down to 1 bug

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,13 +1,40 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link, renderMatches } from "react-router-dom";
-import mediaSearch from '../../utils/lookup';
+import axios from 'axios';
+//import mediaSearch from '../../utils/lookup';
+
 
 function SearchBar() {
+    function lookup(media) {
+        const client = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup';
+        const host = 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com';
+        const key = 'c1ad77c2admsh5b84afab4ae55c6p1137bbjsn2f03e9a2c7cf';
+    
+        var result;
+        const options = {
+            method: 'GET',
+            url: client,
+            params: { term: media },
+            headers: {
+                'X-RapidAPI-Key': key,
+                'X-RapidAPI-Host': host
+            }
+        };
+    
+        axios.request(options).then(function (response) {
+            console.log(response.data);
+            result = response.data;
+        }).catch(function (error) {
+            console.error(error);
+        });
+        return result;
+    }
     document.getElementById("submit").click(function (event) {
         event.preventDefault();
         var search = document.getElementById('search').value;
-        var mediares = mediaSearch(search);
+        var mediares = lookup(search);
+        console.log(mediares);
     })
 
     if (Auth) {
--- client/src/utils/lookup.js
+++ client/src/utils/lookup.js
@@ -1,5 +1,7 @@
+import axios from 'axios'
+import React from 'react';
 import { gql } from '@apollo/client';
-import axios from 'axios';
+
 
 
 //removed for streamlining
